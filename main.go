dbe38ea7bf5c9a8d29a15e3c5cd881282a105931
DJSIer <djsier.dtm@gmail.com>
2019-10-15 15:03:01 +0900

    json format add

--- main.go
+++ main.go
@@ -11,12 +11,7 @@ import (
 
 func main() {
 	lex := lexer.New(`
-	PRG START
-	A LD GR1,GR2
-	LAD GR1,A,GR3
-	ADDA GR1,A
-	A DS 1
-	  DC 1010,16
+	LD GR1,GR2
 	`)
 	p := parser.New(lex)
 	code, err := p.ParseProgram()
@@ -24,7 +19,7 @@ func main() {
 		var buf bytes.Buffer
 		b, _ := json.Marshal(p.Errors())
 		buf.Write(b)
-		fmt.Println("{\"Result\":\"NG\",\"error\" :" + buf.String() + "}")
+		fmt.Println("{\"result\":\"NG\",\"error\" :" + buf.String() + "}")
 		return
 	}
 	code, err = p.LabelToAddress(code)
@@ -32,11 +27,11 @@ func main() {
 		var buf bytes.Buffer
 		b, _ := json.Marshal(p.Errors())
 		buf.Write(b)
-		fmt.Println("{\"Result\":\"NG\",\"error\" :" + buf.String() + "}")
+		fmt.Println("{\"result\":\"NG\",\"error\" :" + buf.String() + "}")
 		return
 	}
 	var buf bytes.Buffer
 	b, _ := json.Marshal(code)
 	buf.Write(b)
-	fmt.Println("{\"Result\":\"OK\",\"code\" :" + buf.String() + "}")
+	fmt.Println("{\"result\":\"OK\",\"code\" :" + buf.String() + "}")
 }
